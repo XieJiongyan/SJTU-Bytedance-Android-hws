5de172215c089f64fe3396155b6bc14cfd5ef412
XieJiongyan <xjy-master@sjtu.edu.cn>
2021-03-24 14:40:17 +0800

    week 4

---
+++ Homework-Chapter-5/app/src/main/java/com/bytedance/practice5/MainActivity.java
@@ -0,0 +1,122 @@
+package com.bytedance.practice5;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.bytedance.practice5.model.Message;
+import com.bytedance.practice5.socket.SocketActivity;
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "chapter5";
+    private FeedAdapter adapter = new FeedAdapter();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Fresco.initialize(this);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        RecyclerView recyclerView = findViewById(R.id.rv_list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+        findViewById(R.id.btn_upload).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,UploadActivity.class);
+                startActivity(intent);
+            }
+        });
+        findViewById(R.id.btn_mine).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getData(Constants.STUDENT_ID);
+            }
+        });
+
+        findViewById(R.id.btn_all).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getData(null);
+            }
+        });
+        findViewById(R.id.btn_socket).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SocketActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+    }
+
+    //TODO 2
+    // 用HttpUrlConnection实现获取留言列表数据，用Gson解析数据，更新UI（调用adapter.setData()方法）
+    // 注意网络请求和UI更新分别应该放在哪个线程中
+    private void getData(String studentId){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String urlStr =
+                        String.format("https://api-sjtu-camp-2021.bytedance.com/homework/invoke/messages?student_id=%s", studentId == null? "" : studentId);
+                List<Message> result = null;
+                try {
+                    URL url = new URL(urlStr);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setConnectTimeout(6000);
+                    conn.setRequestMethod("GET");
+                    conn.setRequestProperty("token", "U0pUVS1ieXRlZGFuY2UtYW5kcm9pZA==");
+                    if (conn.getResponseCode() == 200) {
+                        InputStream in = conn.getInputStream();
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+                        Gson gson = new Gson();
+                        JsonObject jsonobject = gson.fromJson(reader, JsonObject.class);
+                        if (!jsonobject.get("success").getAsBoolean())
+                            Log.i(TAG, "success 字段为false");
+                        result = new Gson().fromJson(jsonobject.get("feeds").getAsJsonArray(), new TypeToken<List<Message>>() {
+                        }.getType());
+                        reader.close();
+                        in.close();
+                        List<Message> finalResult = result;
+                        Log.i(TAG, "okk");
+                        new Handler(getMainLooper()).post(new Runnable() {
+                            @Override
+                            public void run() {
+                                adapter.setData(finalResult);
+                            }
+                        });
+                        adapter.setData(result);
+                    }
+
+                } catch (Exception e) {
+
+                }
+
+            }
+        }).start();
+    }
+
+
+}
