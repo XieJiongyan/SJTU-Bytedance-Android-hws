ef9536f06921a996691d9340ee59e3d5e09e9b87
XieJiongyan <xjy-master@sjtu.edu.cn>
2021-04-02 12:27:44 +0800

    class 5

---
+++ Homework-Chapter-6/app/src/main/java/com/byted/camp/todolist/NoteActivity.java
@@ -0,0 +1,155 @@
+package com.byted.camp.todolist;
+
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatRadioButton;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.byted.camp.todolist.beans.Priority;
+import com.byted.camp.todolist.beans.State;
+import com.byted.camp.todolist.db.TodoContract.TodoNote;
+import com.byted.camp.todolist.db.TodoDbHelper;
+
+
+public class NoteActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private Button addBtn;
+    private RadioGroup radioGroup;
+    private AppCompatRadioButton lowRadio;
+
+    private TodoDbHelper dbHelper;
+    private SQLiteDatabase database;
+    private static int times = 0;
+
+    private boolean is_edit = false;
+    private long id = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note);
+        setTitle(R.string.take_a_note);
+
+        dbHelper = new TodoDbHelper(this);
+        database = dbHelper.getWritableDatabase();
+
+        editText = findViewById(R.id.edit_text);
+        editText.setFocusable(true);
+        editText.setText("todo" + times);
+        times += 1;
+        editText.requestFocus();
+        InputMethodManager inputManager = (InputMethodManager)
+                getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (inputManager != null) {
+            inputManager.showSoftInput(editText, 0);
+        }
+        radioGroup = findViewById(R.id.radio_group);
+        lowRadio = findViewById(R.id.btn_low);
+        lowRadio.setChecked(true);
+
+        Intent intent = getIntent();
+        String msg = intent.getStringExtra("type");
+        if (msg.equals("edit")) {
+            is_edit = true;
+            id = intent.getLongExtra("id", -1);
+        }
+        else
+            is_edit = false;
+        addBtn = findViewById(R.id.btn_add);
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CharSequence content = editText.getText();
+                if (TextUtils.isEmpty(content)) {
+                    Toast.makeText(NoteActivity.this,
+                            "No content to add", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                boolean succeed = false;
+                if (!is_edit) {
+                    succeed = saveNote2Database(content.toString().trim(),
+                            getSelectedPriority());
+                }
+                else {
+                    succeed = updateNode(content.toString().trim(),
+                            getSelectedPriority());
+                    database.close();
+                    database = null;
+                    dbHelper.close();
+                    dbHelper = null;
+                    Intent intent = new Intent(NoteActivity.this, MainActivity.class);
+                    startActivity(intent);
+
+                }
+                if (succeed) {
+                    Toast.makeText(NoteActivity.this,
+                            "Note added/changed", Toast.LENGTH_SHORT).show();
+                    setResult(Activity.RESULT_OK);
+                } else {
+                    Toast.makeText(NoteActivity.this,
+                            "Error", Toast.LENGTH_SHORT).show();
+                }
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        database.close();
+        database = null;
+        dbHelper.close();
+        dbHelper = null;
+
+    }
+
+    private boolean saveNote2Database(String content, Priority priority) {
+        if (database == null || TextUtils.isEmpty(content)) {
+            return false;
+        }
+        ContentValues values = new ContentValues();
+        values.put(TodoNote.COLUMN_CONTENT, content);
+        values.put(TodoNote.COLUMN_STATE, State.TODO.intValue);
+        values.put(TodoNote.COLUMN_DATE, System.currentTimeMillis());
+        values.put(TodoNote.COLUMN_PRIORITY, priority.intValue);
+        long rowId = database.insert(TodoNote.TABLE_NAME, null, values);
+        return rowId != -1;
+    }
+    private boolean updateNode(String content, Priority priority) {
+        if (database == null) {
+            return false;
+        }
+        ContentValues values = new ContentValues();
+        values.put(TodoNote.COLUMN_CONTENT, content);
+        values.put(TodoNote.COLUMN_STATE, State.TODO.intValue);
+        values.put(TodoNote.COLUMN_DATE, System.currentTimeMillis());
+        values.put(TodoNote.COLUMN_PRIORITY, priority.intValue);
+        long rowId = database.update(TodoNote.TABLE_NAME, values,
+                TodoNote._ID + "=" + id, null);
+        return rowId != 0;
+    }
+
+    private Priority getSelectedPriority() {
+        switch (radioGroup.getCheckedRadioButtonId()) {
+            case R.id.btn_high:
+                return Priority.High;
+            case R.id.btn_medium:
+                return Priority.Medium;
+            default:
+                return Priority.Low;
+        }
+    }
+}
---
+++ Homework-Chapter-6/app/src/main/java/com/byted/camp/todolist/ui/NoteViewHolder.java
@@ -0,0 +1,92 @@
+package com.byted.camp.todolist.ui;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.byted.camp.todolist.MainActivity;
+import com.byted.camp.todolist.NoteActivity;
+import com.byted.camp.todolist.NoteOperator;
+import com.byted.camp.todolist.R;
+import com.byted.camp.todolist.beans.Note;
+import com.byted.camp.todolist.beans.State;
+
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+import static android.support.v4.content.ContextCompat.startActivity;
+import static java.security.AccessController.getContext;
+
+public class NoteViewHolder extends RecyclerView.ViewHolder {
+
+    private static final SimpleDateFormat SIMPLE_DATE_FORMAT =
+            new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss", Locale.ENGLISH);
+
+    private final NoteOperator operator;
+
+    private CheckBox checkBox;
+    private TextView contentText;
+    private TextView dateText;
+    private View deleteBtn;
+    private RelativeLayout relanote;
+
+    public NoteViewHolder(@NonNull View itemView, NoteOperator operator) {
+        super(itemView);
+        this.operator = operator;
+
+        checkBox = itemView.findViewById(R.id.checkbox);
+        contentText = itemView.findViewById(R.id.text_content);
+        dateText = itemView.findViewById(R.id.text_date);
+        deleteBtn = itemView.findViewById(R.id.btn_delete);
+        relanote = itemView.findViewById(R.id.note);
+    }
+
+    public void bind(final Note note) {
+        relanote.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Intent intent = new Intent(itemView.getContext(), NoteActivity.class);
+                intent.putExtra("type", "edit");
+                intent.putExtra("id", note.id);
+                itemView.getContext().startActivity(intent);
+                return false;
+            }
+        });
+        contentText.setText(note.getContent());
+        dateText.setText(SIMPLE_DATE_FORMAT.format(note.getDate()));
+
+        checkBox.setOnCheckedChangeListener(null);
+        checkBox.setChecked(note.getState() == State.DONE);
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                note.setState(isChecked ? State.DONE : State.TODO);
+                operator.updateNote(note);
+            }
+        });
+        deleteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                operator.deleteNote(note);
+            }
+        });
+
+        if (note.getState() == State.DONE) {
+            contentText.setTextColor(Color.GRAY);
+            contentText.setPaintFlags(contentText.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+        } else {
+            contentText.setTextColor(Color.BLACK);
+            contentText.setPaintFlags(contentText.getPaintFlags() & ~Paint.STRIKE_THRU_TEXT_FLAG);
+        }
+
+        itemView.setBackgroundColor(note.getPriority().color);
+    }
+}
